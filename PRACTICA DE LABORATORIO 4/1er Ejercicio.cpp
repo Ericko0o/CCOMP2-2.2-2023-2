a3f53c22ffd595012ee93d66ff84d42e16cff8f4
Ericko0o <erick.villacis@ucsp.edu.pe>
2023-09-19 23:57:39 -0500

    Actualizacion 5

--- PRACTICA DE LABORATORIO 4/1er Ejercicio.cpp
+++ PRACTICA DE LABORATORIO 4/1er Ejercicio.cpp
@@ -1,26 +1,15 @@
-#include<iostream>
+#include <iostream>
 
-using namespace std;
-// Ej. si se inserta 10, los 
-//números 10serán 3,5,6 y 9, y su suma será 23.
-bool esMult(int num){
-    if((num%3==0)||(num%5==0)){
-        return true;
-    }
-    return false;
-}
+int main() {
+    int suma = 0;
 
-int main(){
-    int n;
-    int suma=0;
-    cout<<"Número:";
-    cin>>n;
-    for (int i=1;i<n;i++){
-        if (esMult(i)){
-            cout<<i<<" ";
-            suma+=i;
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            suma += i;
         }
     }
-    cout<<"suma"<<suma;
+
+    std::cout << "La suma de los múltiplos de 3 o 5 por debajo de 1000 es: " << suma << std::endl;
+
     return 0;
 }
